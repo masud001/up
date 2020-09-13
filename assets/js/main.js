0f9e82ce50225879f72d3fe3ade36e57621f22d9
masud001 <madhnagar@gmail.com>
2020-09-13 09:28:51 +0600

    the home page almost done!

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,10 +125,10 @@ $(document).ready(function () {
 			600: {
 				items: 1,
 			},
-			900: {
+			992: {
 				items: 2,
 			},
-			1000: {
+			1200: {
 				items: 3,
 			},
 		},
@@ -153,7 +153,7 @@ $(document).ready(function () {
 			600: {
 				items: 1,
 			},
-			900: {
+			992: {
 				items: 2,
 			},
 			1200: {
@@ -181,7 +181,7 @@ $(document).ready(function () {
 			600: {
 				items: 1,
 			},
-			900: {
+			992: {
 				items: 2,
 			},
 			1200: {
@@ -209,7 +209,7 @@ $(document).ready(function () {
 			600: {
 				items: 1,
 			},
-			900: {
+			992: {
 				items: 2,
 			},
 			1200: {
@@ -260,3 +260,19 @@ $(document).ready(function () {
 		// other options
 	});
 });
+
+const preloader = document.querySelector(".preloader");
+
+const fadeEffect = setInterval(() => {
+	// if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
+	if (!preloader.style.opacity) {
+		preloader.style.opacity = 1;
+	}
+	if (preloader.style.opacity > 0) {
+		preloader.style.opacity -= 0.1;
+	} else {
+		clearInterval(fadeEffect);
+	}
+}, 200);
+
+window.addEventListener("load", fadeEffect);
